dce287f90102ff306280fcf7b473a3c352401ef5
Oleg Smedyuk <conformist.mw@gmail.com>
2021-08-23 17:23:11 +0300

    respect browser bounds to optimize buses count

--- server.py
+++ server.py
@@ -16,16 +16,38 @@ logging.basicConfig(
 logger = logging.getLogger(__file__)
 
 sender, receiver = trio.open_memory_channel(0)
+bounds_sender, bounds_receiver = trio.open_memory_channel(0)
 
 
-async def send_to_browser(request):
-    ws = await request.accept()
-    start, buses = monotonic(), []
+def is_inside(bounds, lat, lng):
+    if bounds is None:  # have not yet received data from the browser
+        return True
+    return (
+        (bounds['south_lat'] < lat < bounds['north_lat'])
+        and (bounds['west_lng'] < lng < bounds['east_lng'])
+    )
+
+
+async def listen_browser(ws):
+    while True:
+        msg = json.loads(await ws.get_message())
+        logger.info(msg)
+        if msg['msgType'] == 'newBounds':
+            await bounds_sender.send(msg['data'])
+
+
+async def send_to_browser(ws):
+    start, buses, bounds = monotonic(), [], None
     while True:
         try:
+            try:
+                bounds = bounds_receiver.receive_nowait()
+            except trio.WouldBlock:
+                pass
             bus = await receiver.receive()
-            buses.append(bus)
-            if (monotonic() - start) > 3 and buses:
+            if is_inside(bounds, bus['lat'], bus['lng']):
+                buses.append(bus)
+            if (monotonic() - start) > 1:
                 await ws.send_message(json.dumps({
                     'msgType': 'Buses',
                     'buses': buses,
@@ -33,6 +55,13 @@ async def send_to_browser(request):
                 start, buses = monotonic(), []
         except ConnectionClosed:
             break
+
+
+async def interact_with_browser(request):
+    ws = await request.accept()
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(send_to_browser, ws)
+        nursery.start_soon(listen_browser, ws)
 
 
 async def listen_fake_events(request):
@@ -50,7 +79,7 @@ async def main():
         nursery.start_soon(
             partial(
                 serve_websocket,
-                send_to_browser,
+                interact_with_browser,
                 '127.0.0.1',
                 8000,
                 ssl_context=None,

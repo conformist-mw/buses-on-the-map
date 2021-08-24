349876643b32532ca45c0af7ac96837e9ae5be15
Oleg Smedyuk <conformist.mw@gmail.com>
2021-08-24 16:14:39 +0300

    added error handling to the server

--- server.py
+++ server.py
@@ -13,13 +13,17 @@ from models import Bus, BusEncoder, WindowBounds
 # noinspection PyArgumentList
 logging.basicConfig(
     level=logging.INFO,
-    format='{asctime} - {levelname} - {message}',
+    format='{asctime} - {levelname} - {message} : {lineno}',
     style='{',
 )
 logger = logging.getLogger(__file__)
 
 sender, receiver = trio.open_memory_channel(0)
 bounds_sender, bounds_receiver = trio.open_memory_channel(0)
+
+
+class EmptyMessage(Exception):
+    ...
 
 
 def parse_args():
@@ -46,13 +50,48 @@ def parse_args():
     return parser.parse_args()
 
 
-async def listen_browser(ws):
-    while True:
-        msg = json.loads(await ws.get_message())
-        logger.info(msg)
-        if msg['msgType'] == WindowBounds.msg_type:
-            await bounds_sender.send(msg['data'])
+async def check_message(encoded_message):
+    message = None
+    error_msg = None
+    try:
+        message = json.loads(encoded_message)
+        if message is None:
+            raise EmptyMessage
+        if isinstance(message, dict) and 'msgType' not in message:
+            raise KeyError
+    except json.JSONDecodeError:
+        error_msg = 'Requires valid JSON'
+    except KeyError:
+        error_msg = 'Requires msgType specified'
+    except EmptyMessage:
+        error_msg = 'Message is null'
 
+    return message, error_msg
+
+
+async def send_error(ws, error_message, source_message):
+    error = json.dumps({
+        'errors': [error_message],
+        'msgType': 'Errors',
+        'source_msg': source_message,
+    })
+    await ws.send_message(error)
+
+
+async def listen_to_browser(ws):
+    while True:
+        try:
+            encoded_message = await ws.get_message()
+            logger.debug(encoded_message)
+            message, error_message = await check_message(encoded_message)
+            if error_message:
+                await send_error(ws, error_message, encoded_message)
+                continue
+            if message['msgType'] == WindowBounds.msg_type:
+                await bounds_sender.send(message['data'])
+        except ConnectionClosed:
+            logger.debug('listen_to_browser: connection closed')
+            break
 
 async def send_to_browser(ws):
     start, buses, bounds = monotonic(), [], WindowBounds()
@@ -76,16 +115,22 @@ async def interact_with_browser(request):
     ws = await request.accept()
     async with trio.open_nursery() as nursery:
         nursery.start_soon(send_to_browser, ws)
-        nursery.start_soon(listen_browser, ws)
+        nursery.start_soon(listen_to_browser, ws)
 
 
 async def listen_fake_events(request):
     ws = await request.accept()
     while True:
         try:
-            message = await ws.get_message()
-            await sender.send(Bus.from_json(message))
+            encoded_message = await ws.get_message()
+            logger.debug(encoded_message)
+            message, error_message = await check_message(encoded_message)
+            if error_message:
+                await send_error(ws, error_message, encoded_message)
+                continue
+            await sender.send(Bus(**message))
         except ConnectionClosed:
+            logger.debug('listen_fake_events: connection closed')
             break
 
 
